b74e355db02cda1111b354be675b2ed3626704b2
Heitor <halbuquerquevieira@gmail.com>
2012-12-02 18:03:43 -0200

    login

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,23 @@ var express = require('express'),
 var app = express();
 app.set('views', __dirname + '/../views');
 app.use(express.bodyParser());
+
 app.post('/sandbox/:id/run',sandbox.run);
 app.post('/sandbox/:id/save',sandbox.save);
+app.post('/login',sandbox.login_post);
+app.post('/signup',sandbox.signup_post);
+app.post('/lost-password',sandbox.lost_password_post);
+app.post('/reset-password',sandbox.reset_password_post);
+app.post('/delete',sandbox.delete);
+
 app.get('/sandbox/:id',sandbox.sandbox);
 app.get('/sandbox',sandbox.new_sandbox);
+app.get('/login',sandbox.login_get);
+app.get('/signup',sandbox.signup_get);
+app.get('/lost-password',sandbox.lost_password_get);
+app.get('/reset-password',sandbox.reset_password_get);
+app.get('/print',sandbox.print);
+app.get('/reset',sandbox.reset);
 app.get('/',sandbox.home_view);
 
 module.exports = app;
